b85a6149f45c52b7e9c47a558d073a5eab0d98d9
WebDevNoob <thewebdevnoob@gmail.com>
2016-01-15 22:25:27 -0500

    Moved params to articles_helper

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -40,8 +40,4 @@ class ArticlesController < ApplicationController
     redirect_to articles_path
   end
 
-  private
-  def article_params
-    params.require(:article).permit(:title,:text)
-  end
 end
